b603d2de3d0317fbcdefb928c8d4cc4d1aaca38b
Miroslaw Chmielecki <misiak19@op.pl>
2019-11-05 11:10:47 +0100

    Add basic structure

---
+++ src/containers/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import sudoku from "sudoku-umd";
+import { hot } from "react-hot-loader";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
+  render() {
+    return;
+  }
+}
+
+export default hot(module)(App);
